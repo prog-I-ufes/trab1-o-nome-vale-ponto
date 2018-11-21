1fea13c50620f4fdd49def3c5d39ceb3ffa3a690
lucaSalgado <lucas.gi.salgado@gmail.com>
2018-11-21 14:29:40 -0200

    recebe o config.txt sem problemas

--- trabalho.c
+++ trabalho.c
@@ -14,20 +14,18 @@ void testeAbrirArquivo(FILE * conf){
 void recebeConfig(FILE *conf, char *treino, char *teste, char *predicao, int x, int *k, char *tipo, float *r){
         conf = fopen("bateria_validacao/iris/config.txt", "r");
         fscanf(conf, "%s\n%s\n%s", treino, teste, predicao);
-        printf("%s\n",treino);
-        printf("%s\n",teste);
-        printf("%s\n",predicao);
+     //   printf("%s\n",treino);
+     //   printf("%s\n",teste);
+     //   printf("%s\n",predicao);
 		int w=0;
-		printf("o valor de x eh igual a %d\n",x);
-		k = (int *)malloc((x-3) * sizeof(int));
-		r = (float *)malloc((x-3) * sizeof(float));
-		tipo = (char *)malloc((x-3) * sizeof(char));
-		printf("\n");
+		//printf("o valor de x eh igual a %d\n",x);
+	//	printf("\n");
 		for(w=3;w<x;w++)
 		{	
 		fscanf(conf, "%d, %c, %f",&k[(w-3)], &tipo[(w-3)], &r[(w-3)]);
-		printf("%d %c %.2f\n",k[(w-3)], tipo[(w-3)], r[(w-3)]);
+		//printf("%d %c %.2f\n",k[(w-3)], tipo[(w-3)], r[(w-3)]);
 		}
+      fclose(conf);
 	return;
 }
 
@@ -39,33 +37,27 @@ int contaLinhas(FILE *conf){
 	x++;
         }
 	x--;
-	printf("o arquivo tem %d linhas\n",x);
+//	printf("o arquivo tem %d linhas\n",x);
         return x;
-}
-
-void liberarMemoria(FILE *conf, int *k, float *r, char *tipo){
-
-	fclose(conf);
-	//free(k);
-	//free(r);
-	//free(tipo);
-	return;
 }
 
 int main (){
         FILE *conf; // *conf é um ponteiro do tipo arquivo para o arquivo config.txt
         int x=0, *k; // x é uma variável para contar quantas linhas config.txt possui; *k é um ponteiro para um vetor dinâmico que armazena os valores de k
-	float *r; // *r é um ponteiro para um vetor dinãmico que armazena os valores de r quando eles existem
+	     float *r; // *r é um ponteiro para um vetor dinãmico que armazena os valores de r quando eles existem
         char treino[30], teste[30], predicao[30], *tipo; //
         conf = fopen("bateria_validacao/iris/config.txt", "r");
         
         testeAbrirArquivo(conf);
         
         x = contaLinhas(conf);
-
+         k = (int *)malloc((x-3) * sizeof(int));
+		   r = (float *)malloc((x-3) * sizeof(float));
+		   tipo = (char *)malloc((x-3) * sizeof(char));
         recebeConfig(conf, treino, teste, predicao, x, k, tipo, r);
 
-	liberarMemoria(conf, k, r, tipo);
-        
+free(k); // os free estão funcionando, pois os frees tem que estar na mesma função que fez a malloc
+free(r);
+free(tipo);       
 return 0;
 }
