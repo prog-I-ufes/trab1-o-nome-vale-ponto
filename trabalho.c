858e7343cc1255c61733af5f7d206e9a18e4a649
lucaSalgado <lucas.gi.salgado@gmail.com>
2018-11-23 20:01:36 -0200

    faz a leitura do tamanho tanto do irirs quanto do vowels

--- trabalho.c
+++ trabalho.c
@@ -16,12 +16,12 @@ void testeAbrirArquivo(FILE * conf){ // a função tenta abrir o arquivo config.
 void recebeConfig(FILE *conf, char *treino, char *teste, char *predicao, int x, int *k, char *tipo, float *r){ // a função recebe todas as linhas do arquivo config.txt
         conf = fopen(CONFIG, "r");
         fscanf(conf, "%s\n%s\n%s", treino, teste, predicao);
-// printf("\n%s\n",treino);
-// printf("\n%s\n",teste);
-//  printf("\n%s\n",predicao);
+/*printf("\n%s\n",treino);
+printf("\n%s\n",teste);
+printf("\n%s\n",predicao);*/
 		int w=0;
 //printf("o valor de x eh igual a %d\n",x);
-//	printf("\n");
+//printf("\n");
 		for(w=3;w<x;w++)
 		{	
 		fscanf(conf, "%d, %c, %f",&k[(w-3)], &tipo[(w-3)], &r[(w-3)]);
@@ -31,94 +31,120 @@ void recebeConfig(FILE *conf, char *treino, char *teste, char *predicao, int x, 
 	return;
 }
 
-int contaConfig(FILE *conf){ // a funçao conta quantas linhas config.txt possui
-        char tx1[30]=" "; 
-        int x=0;       
+int contaConfig(FILE *conf, int *tamTreino, int *tamTeste, int* tamPredicao){ // a funçao conta quantas linhas config.txt possui
+        char letra; 
+        int x=0, z =0;       
         while(!feof(conf)){
-        fgets(tx1, 30, conf);
-	x++;
+        fscanf(conf, "%c", &letra);
+                if (letra == '\n')
+                {
+                        x++;
+                }
         }
         x--;
-        //printf("o arquivo tem %d linhas\n",x);
+//printf("o arquivo tem %d linhas\n",x);
+        char le;
+        rewind(conf);
+                while(!feof(conf)){
+        fscanf(conf, "%c", &le);
+                if (le == '\n')
+                {
+                        z++;
+                }
+                if (z==0)
+                {
+                        *tamTreino = *tamTreino + 1;
+                }
+                if (z==1)
+                {
+                        *tamTeste = *tamTeste + 1;
+                }
+                if (z==2)
+                {
+                        *tamPredicao = *tamPredicao + 1;
+                }
+        }
+        *tamTreino = *tamTreino + 1;
+        *tamTeste = *tamTeste + 1;
+        *tamPredicao = *tamPredicao + 1;
+/*printf("\no arquivo tem %d linhas\n",x);
+printf("\nteste tem %d caracteres\n",*tamTeste);
+printf("\ntreino tem %d caracteres\n",*tamTreino);
+printf("\npredicao tem %d caracteres\n",*tamPredicao);*/
         return x;
 }
 
-void contaCSV(char *treino, char *teste, FILE *csvteste, FILE *csvtreino, int *linTeste, int *colTeste, int *linTreino, int *colTreino){ // a função lê o número de linhas e colunas do teste e do treino
-        csvteste = fopen (teste,"r");
-        char tx1[30], letra;  
-        while(!feof(csvteste)){
-                fgets(tx1, 30, csvteste);   // faz leitura do número de linhas do teste.csv
-                *linTeste = *linTeste + 1;
+void contaCSV(char *local, FILE *csvConta, long int *linConta, long int *colConta){ // a função lê o número de linhas e colunas do teste e do treino
+        csvConta = fopen (local,"r");
+        char letra;  
+        while(!feof(csvConta)){
+                fscanf(csvConta, "%c", &letra); // faz leitura do número de linhas
+                if (letra == '\n')
+                {
+                        *linConta = *linConta + 1;
+                }  
         }
-        *linTeste = *linTeste - 1;
+        *linConta = *linConta - 1; // ele conta uma linha a mais
         
-        csvteste = fopen (teste,"r");
-        while(!feof(csvteste)){
-                fscanf(csvteste, "%c", &letra);
+        csvConta = fopen (local,"r");
+        while(!feof(csvConta)){
+                fscanf(csvConta, "%c", &letra);
                 if (letra == ',')
                 {
-                        *colTeste = *colTeste + 1; // faz leitura do número de colunas do teste.csv
+                        *colConta = *colConta + 1; // faz leitura do número de colunas
                 }
                 if (letra == '\n')
                 {
                         break;
                 }  
         }
-        *colTeste = *colTeste + 1; //o numero de caracteristicas é o numero de virgulas +1 
-        // printf("\n\n\n %d eh o numero de linhas\n\n\n",linTeste);
-        // printf("\n\n\n %d eh o numero de colunas\n\n\n",colTeste);
-  
-        csvtreino = fopen (treino,"r");
-        while(!feof(csvtreino)){
-                fgets(tx1, 30, csvtreino);   // faz leitura do número de colunas do treino.csv
-                *colTreino = *colTreino + 1;
-        }
-        *colTreino = *colTreino - 1;
-        
-        csvtreino = fopen (teste,"r");
-        while(!feof(csvtreino)){
-                fscanf(csvtreino, "%c", &letra);
-                if (letra == ',')
-                {
-                        *linTreino = *linTreino + 1; // faz leitura do número de linhas do treino.csv
-                }
-                if (letra == '\n')
-                {
-                        break;
-                }  
-        }
-        *linTreino = *linTreino + 1; //o numero de caracteristicas é o numero de virgulas +1
-
-        fclose(csvtreino);
-        fclose(csvteste);
+        *colConta = *colConta + 1; //o numero de caracteristicas é o numero de virgulas +1 
+// printf("\n\n\n %d eh o numero de 0 linhas\n\n\n",*linConta);
+// printf("\n\n\n %d eh o numero de 0 colunas\n\n\n",*colConta);
+        fclose(csvConta);
         return;
 }
 
 
 
+
 int main (){
         FILE *conf, *csvteste, *csvtreino; // *conf é um ponteiro do tipo arquivo para o arquivo config.txt
-        int x=0, *k, linTeste = 0, colTeste = 0, colTreino = 0, linTreino = 0; // x é uma variável para contar quantas linhas config.txt possui; *k é um ponteiro para um vetor dinâmico que armazena os valores de k
-        int *liNTeste = &linTeste, *coLTeste = &colTeste, *coLTreino = &colTreino, *liNTreino = &linTreino;
+      long int  linTeste = 0, colTeste = 0, colTreino = 0, linTreino = 0;
+        int tamTeste = 0, tamTreino = 0, tamPredicao = 0; 
+      long  int *liNTeste = &linTeste, *coLTeste = &colTeste, *coLTreino = &colTreino, *liNTreino = &linTreino; // declara ponteiros do tipo int
+        int x=0, *k;// x é uma variável para contar quantas linhas config.txt possui; *k é um ponteiro para um vetor dinâmico que armazena os valores de k
 	float *r; // *r é um ponteiro para um vetor dinãmico que armazena os valores de r quando eles existem
-        char treino[30], teste[30], predicao[30], *tipo; // Strings que armazenam os endereços em que paramêtros estão e um vetor de caracteres para a configuração dos calculos
+        char *treino, *teste, *predicao, *tipo; // Strings que armazenam os endereços em que paramêtros estão e um vetor de caracteres para a configuração dos calculos
         conf = fopen(CONFIG, "r");
         
         testeAbrirArquivo(conf);
         
-        x = contaConfig(conf);
+        x = contaConfig(conf, &tamTreino, &tamTeste, &tamPredicao);
+
             k = (int *)malloc((x-3) * sizeof(int));
             r = (float *)malloc((x-3) * sizeof(float)); // faz a alocação dos vetores que armazenarão os valores para configurar os calculos
             tipo = (char *)malloc((x-3) * sizeof(char));
+
+            teste = (char *)malloc((tamTeste) * sizeof(char));
+            treino = (char *)malloc((tamTreino) * sizeof(char)); // faz a alocação dos vetores que armazenarão os valores para configurar os calculos
+            predicao = (char *)malloc((tamPredicao) * sizeof(char));
+        
          recebeConfig(conf, treino, teste, predicao, x, k, tipo, r);
-         contaCSV(treino, teste, csvteste, csvtreino, liNTeste, coLTeste, coLTreino, liNTreino);
-  /*       printf("\n\n\n %d eh o numero de linhas\n\n\n",linTeste);
-         printf("\n\n\n %d eh o numero de colunas\n\n\n",colTeste);
-         printf("\n\n\n %d eh o numero de linhas\n\n\n",linTreino);
-         printf("\n\n\n %d eh o numero de colunas\n\n\n",colTreino);*/
+         contaCSV(teste, csvteste, liNTeste, coLTeste);
+
+         contaCSV(treino, csvtreino, liNTreino,coLTreino );
+/*printf("\n\n\n %ld eh o numero de linhas do teste\n\n\n",linTeste);
+printf("\n\n\n %ld eh o numero de colunas do teste\n\n\n",colTeste);    
+printf("\n\n\n %ld eh o numero de 2 linhas do treino\n\n\n",linTreino);
+printf("\n\n\n %ld eh o numero de 2 colunas do treino\n\n\n",colTreino);*/
+   
+   free(treino);
+   free(teste);
+   free(predicao);
    free(k); 
    free(r);
    free(tipo);
-        //liberaMemoria(kk, rr, titipo);       
+      
 return 0;
 }
